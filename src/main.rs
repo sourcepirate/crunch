26c74ec44c849703017faef6009b66282fa9cdcd
sourcepirate <sathyanarrayanan@yandex.com>
2020-05-26 23:32:37 +0800

    updated cargo lint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 //! This simple tools is used to strip the common sequnce between files
 //! in the given directory
-//! 
+//!
 //! Uses LCS algorithm to remove sequence strings from the given
 //! text file.
-//! 
+//!
 
 pub mod lcs;
 
-fn main() {
-    
-}
+fn main() {}
